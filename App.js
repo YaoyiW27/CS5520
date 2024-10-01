12a82690e5f0ff6f49326150b5fabff813d5b731
YaoyiW27 <rmwyyy@gmail.com>
2024-10-01 13:38:21 -0700

    added StackScreen for Homepage

--- App.js
+++ App.js
@@ -2,11 +2,17 @@ import React from 'react'
 import { Text, View } from 'react-native'
 import Home from './components/Home'
 import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+console.log(Stack);
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Home />
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
     </NavigationContainer>
   )
 }
