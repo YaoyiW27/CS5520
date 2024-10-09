eed2416fa9235bed9e431fb3739c694d11ff5b6c
YaoyiW27 <rmwyyy@gmail.com>
2024-10-08 21:53:11 -0400

    added pressable in app.js

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Home from "./components/Home";
+import GoalDetails from "./components/GoalDetails";
+import PressableButton from "./components/PressableButton"; 
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import GoalDetails from "./components/GoalDetails";
-import { Button } from "react-native";
+import { Text } from "react-native";
 
 const Stack = createNativeStackNavigator();
 
@@ -27,20 +28,15 @@ export default function App() {
         <Stack.Screen
           name="Details"
           component={GoalDetails}
-          options={({ route }) => {
-            return {
-              ...headerOptions,
-              title: route.params ? route.params.goalData.text : "More Details",
-              headerRight: () => (
-                <Button
-                  title="Warning"
-                  onPress={() => {
-                    console.log("warning");
-                  }}
-                />
-              ),
-            };
-          }}
+          options={({ route }) => ({
+            ...headerOptions,
+            title: route.params ? route.params.goalData.text : "More Details",
+            headerRight: () => (
+              <PressableButton pressedFunction={() => console.log("warning")}>
+                <Text style={{ color: 'white', fontSize: 16 }}>Warning</Text>
+              </PressableButton>
+            ),
+          })}
         />
       </Stack.Navigator>
     </NavigationContainer>
