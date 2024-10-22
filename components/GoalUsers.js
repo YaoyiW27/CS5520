1dba219c57c626b90c6ce1491d8d58ff8b19863b
YaoyiW27 <rmwyyy@gmail.com>
2024-10-22 14:12:17 -0700

    render users using flatlist

--- components/GoalUsers.js
+++ components/GoalUsers.js
@@ -1,24 +1,46 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React, { useEffect } from 'react'
+import { View, Text, StyleSheet, FlatList } from 'react-native'
+import React, { useEffect , useState
+ } from 'react'
 
 export default function GoalUsers() {
+    const [users, setUsers] = useState([]);
+
     useEffect( () => {
         // fetch data
         async function fetchData() {
             try {
             const response = await fetch(
                 'https://jsonplaceholder.typicode.com/users'
-            )
-        }catch(err)
-        {console.log("fetch user data", err);
-        }
+            );
+            if (!response.ok) {
+                throw new Error(
+                    `An HTTP error happenend with status: ${response.status}`
+                );
+            }
+            // this code wukk only run if the response is ok
+            // extract data
+            const data = await response.json();
+            // set the users state variable
+            setUsers (
+               data.map ((user)=> {
+                return user.name;
+               })
+            );
+            }catch(err)
+                {console.log("fetch user data", err);
+            }
             fetchData();
         }
         fetchData();
     }, []);
   return (
     <View>
-      <Text>GoalUsers</Text>
+        <FlatList 
+            data={users} 
+            renderItem={({item}) => {
+            return <Text>{item}</Text>;
+            }}
+        />
     </View>
   )
 }
